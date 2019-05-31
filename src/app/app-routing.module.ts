e0fd188174e073111aeff180ce57cee3e154accf
dat.tieu <tieu.dat102@gmail.com>
2019-05-31 15:54:16 +0700

    init routes, layouts, pages and components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { LoginLayoutComponent } from './layouts/login-layout/login-layout.component';
+
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { VideoPageComponent } from './pages/video-page/video-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: VideoPageComponent }
+    ]
+  }
+  ,
+  {
+    path: 'login',
+    component: LoginLayoutComponent,
+    children: [{path: '', component: LoginPageComponent }]
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { LoginLayoutComponent } from './layouts/login-layout/login-layout.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { VideoPageComponent } from './pages/video-page/video-page.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { VideoViewComponent } from './components/video-view/video-view.component';
+import { VideoSearchComponent } from './components/video-search/video-search.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    LoginLayoutComponent,
+    LoginPageComponent,
+    VideoPageComponent,
+    LoginFormComponent,
+    VideoViewComponent,
+    VideoSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
