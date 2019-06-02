14546ba7b134c590f96c902ffea953e482072011
dat.tieu <tieu.dat102@gmail.com>
2019-06-02 16:23:01 +0700

    add home page

---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent implements OnInit {
+  constructor(private user: UserService) { }
+
+  ngOnInit() {
+  }
+
+}
