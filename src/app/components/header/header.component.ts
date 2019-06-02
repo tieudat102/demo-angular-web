66ed29efff3a7a47104e1a1b929a53a2f1f72d11
dat.tieu <tieu.dat102@gmail.com>
2019-06-02 16:23:14 +0700

    add header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  isLogin: boolean;
+  constructor(private user: UserService) { }
+
+  ngOnInit() {
+    // isLogin = this.user.isLogin();
+  }
+
+}
